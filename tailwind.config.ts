f9c26644b8bee80a404563c80a370a6e4e88c36b
tsomic <keller.nils99@gmail.com>
2024-03-12 22:15:00 +0100

    🔧 remove default colors from tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,11 @@ export default {
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#fafafa",
-        secondary: "#27272a",
-        background: "#09090b",
-        "muted-foreground": "#a1a1aa",
-      },
+    colors: {
+      primary: "#fafafa",
+      secondary: "#27272a",
+      background: "#09090b",
+      "muted-foreground": "#a1a1aa",
     },
   },
 } satisfies Config;
