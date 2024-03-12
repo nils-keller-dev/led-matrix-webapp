1335bacd164cfff25795ab7c0e02c1e027b8da78
tsomic <keller.nils99@gmail.com>
2024-03-12 22:15:39 +0100

    💄 refactor UI layout

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,14 +1,14 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
-    <html>
+    <html class="h-full overflow-hidden">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>led-matrix-webapp</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
-      <body>
+      <body class="h-full">
         <Component />
       </body>
     </html>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,34 +1,24 @@
-import { useSignal } from "@preact/signals";
+import { Sun, SunDim } from "https://esm.sh/lucide-preact";
 import { Slider } from "../islands/Slider.tsx";
 
 export default function Home() {
-  const count = useSignal(3);
-
   const handleSliderChange = (newValue: number) => {
     console.log("Slider value:", newValue);
   };
 
   return (
-    <div class="px-4 py-8 mx-auto bg-background text-primary">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Slider
-          min={0}
-          max={100}
-          onChange={handleSliderChange}
-        >
-        </Slider>
+    <div class="bg-background text-primary h-full flex items-end">
+      <div class="w-full p-7 pt-4 rounded-t-xl border border-secondary border-b-0">
+        <div class="bg-secondary w-24 h-2 rounded-full mx-auto mb-7" />
+        <span class="flex gap-3">
+          <SunDim class="text-muted-foreground shrink-0" />
+          <Slider
+            min={0}
+            max={100}
+            onChange={handleSliderChange}
+          />
+          <Sun class="text-muted-foreground shrink-0" />
+        </span>
       </div>
     </div>
   );
