df525df370482e47768a332f69833b3efba17367
tsomic <keller.nils99@gmail.com>
2024-03-13 17:18:32 +0100

    ✨ add Carousel items and activeItem indicator

--- islands/Carousel.tsx
+++ islands/Carousel.tsx
@@ -1,21 +1,61 @@
-import { ArrowLeft, ArrowRight } from "https://esm.sh/lucide-preact";
+import { useSignal } from "@preact/signals";
+import { ArrowLeft, ArrowRight, Settings2 } from "https://esm.sh/lucide-preact";
 import { IconButton } from "../components/IconButton.tsx";
-import { Settings2 } from "https://esm.sh/lucide-preact";
 
-export function Carousel() {
+interface CarouselItem {
+  title: string;
+  hasSettingsIcon: boolean;
+}
+
+interface CarouselProps {
+  items: CarouselItem[];
+}
+
+export function Carousel({
+  items,
+}: CarouselProps) {
+  const activeItem = useSignal(0);
+
+  const nextItem = () => {
+    activeItem.value = (activeItem.value + 1) % items.length;
+  };
+
+  const previousItem = () => {
+    activeItem.value = (activeItem.value - 1 + items.length) % items.length;
+  };
+
   return (
     <div class="w-full p-7 aspect-square">
       <div class="text-7xl font-abril size-full  border border-secondary rounded-3xl flex items-center justify-center relative">
-        Clock
-        <IconButton class="absolute top-1/2 left-0 -translate-x-1/2 -translate-y-1/2">
+        {items[activeItem.value].title}
+        <IconButton
+          class="absolute top-1/2 left-0 -translate-x-1/2 -translate-y-1/2"
+          onClick={previousItem}
+        >
           <ArrowLeft />
         </IconButton>
-        <IconButton class="absolute top-1/2 right-0 translate-x-1/2 -translate-y-1/2">
+        <IconButton
+          class="absolute top-1/2 right-0 translate-x-1/2 -translate-y-1/2"
+          onClick={nextItem}
+        >
           <ArrowRight />
         </IconButton>
-        <IconButton class="absolute top-2 right-2">
-          <Settings2 />
-        </IconButton>
+        {items[activeItem.value].hasSettingsIcon && (
+          <IconButton class="absolute top-2 right-2">
+            <Settings2 />
+          </IconButton>
+        )}
+      </div>
+      <div class="flex justify-center gap-1 mt-2">
+        {items.map((item, index) => (
+          <div
+            class={`size-2 rounded-full ${
+              index === activeItem.value
+                ? "bg-muted-foreground"
+                : "bg-secondary"
+            }`}
+          />
+        ))}
       </div>
     </div>
   );
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,23 +1,41 @@
 import { Sun, SunDim } from "https://esm.sh/lucide-preact";
-import { Slider } from "../islands/Slider.tsx";
 import { Carousel } from "../islands/Carousel.tsx";
+import { Slider } from "../islands/Slider.tsx";
 
 export default function Home() {
-  const handleSliderChange = (newValue: number) => {
-    console.log("Slider value:", newValue);
-  };
+  const carouselItems = [
+    {
+      title: "Clock",
+      hasSettingsIcon: true,
+    },
+    {
+      title: "Music",
+      hasSettingsIcon: false,
+    },
+    {
+      title: "Image",
+      hasSettingsIcon: true,
+    },
+    {
+      title: "Text",
+      hasSettingsIcon: true,
+    },
+    {
+      title: "Idle",
+      hasSettingsIcon: false,
+    },
+  ];
 
   return (
-    <div class="bg-background text-primary h-full flex flex-col justify-between">
-      <Carousel />
-      <div class="w-full p-7 pt-4 rounded-t-xl border border-secondary border-b-0">
+    <div class="bg-background text-primary h-full flex items-center">
+      <Carousel items={carouselItems} />
+      <div class="w-full p-7 pt-4 rounded-t-xl border border-secondary border-b-0 fixed bottom-0 bg-background">
         <div class="bg-secondary w-24 h-2 rounded-full mx-auto mb-7" />
         <span class="flex gap-3 flex-row">
           <SunDim class="text-muted-foreground shrink-0" />
           <Slider
             min={0}
             max={100}
-            onChange={handleSliderChange}
           />
           <Sun class="text-muted-foreground shrink-0" />
         </span>
