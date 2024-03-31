a63bec51e3968ad94940ca832c610b5d5e50b36e
tsomic <keller.nils99@gmail.com>
2024-03-31 13:56:33 +0200

    ✨ add loading animation to images

--- src/components/settingGroup/Image.tsx
+++ src/components/settingGroup/Image.tsx
@@ -1,8 +1,54 @@
 import { useSignal } from '@preact/signals'
-import { Plus } from 'lucide-preact'
+import { LoaderCircle, Plus } from 'lucide-preact'
 import { deleteImage } from '../../api/image.delete'
 import { postImage } from '../../api/image.post'
 import { postJson } from '../../api/json.post'
+
+type ImageItemSettingsProps = {
+  image: string
+  isSelected: boolean
+  onDelete: (image: string) => void
+  onSelect: (image: string) => void
+}
+
+function ImageItem({
+  image,
+  isSelected,
+  onDelete,
+  onSelect
+}: ImageItemSettingsProps) {
+  const isLoading = useSignal(true)
+
+  const onContextMenu = (e: Event, image: string) => {
+    e.preventDefault()
+    if (window.confirm('Are you sure you want to delete this image?')) {
+      onDelete(image)
+    }
+  }
+
+  const onLoad = () => {
+    isLoading.value = false
+  }
+
+  return (
+    <div
+      className={`w-full aspect-square rounded-xl overflow-hidden relative ${isSelected ? 'border-2 border-primary' : 'border border-secondary'}`}
+    >
+      {isLoading.value && (
+        <div className="size-full flex justify-center items-center">
+          <LoaderCircle className="size-5 animate-spin" />
+        </div>
+      )}
+      <img
+        src={`image/${image}`}
+        className={`size-full object-contain ${isLoading.value ? 'opacity-0' : 'opacity-100'}`}
+        onClick={() => onSelect(image)}
+        onContextMenu={(e) => onContextMenu(e, image)}
+        onLoad={onLoad}
+      />
+    </div>
+  )
+}
 
 type ImageSettingsProps = {
   image: string
@@ -13,23 +59,11 @@ export function Image({ image, images }: ImageSettingsProps) {
   const currentImage = useSignal(image)
   const imageList = useSignal(images)
 
-  const updateImage = (image: string) => {
-    postJson({ image })
-    currentImage.value = image
-  }
-
   const startDrag = (e: MouseEvent | TouchEvent) => {
     e.stopPropagation()
   }
 
-  const onContextMenu = (e: Event, image: string) => {
-    e.preventDefault()
-    if (
-      !window.confirm('Are you sure you want to permanently delete this image?')
-    ) {
-      return
-    }
-
+  const onDeleteImage = (image: string) => {
     deleteImage(image)
 
     imageList.value = imageList.value.filter((imageName) => imageName !== image)
@@ -38,6 +72,11 @@ export function Image({ image, images }: ImageSettingsProps) {
       currentImage.value = imageList.value[0]
       postJson({ image: currentImage.value })
     }
+  }
+
+  const updateImage = (image: string) => {
+    postJson({ image })
+    currentImage.value = image
   }
 
   const uploadFile = (e: Event) => {
@@ -62,12 +101,12 @@ export function Image({ image, images }: ImageSettingsProps) {
     >
       <div className="grid grid-cols-3 gap-3">
         {imageList.value.map((imageName, index) => (
-          <img
-            src={`image/${imageName}`}
+          <ImageItem
             key={index}
-            className={`w-full aspect-square object-contain rounded-xl ${imageName === currentImage.value ? 'border-2 border-primary' : 'border border-secondary'}`}
-            onClick={() => updateImage(imageName)}
-            onContextMenu={(e) => onContextMenu(e, imageName)}
+            image={imageName}
+            isSelected={imageName === currentImage.value}
+            onDelete={onDeleteImage}
+            onSelect={updateImage}
           />
         ))}
         <div className="w-full aspect-square rounded-xl border border-dashed border-muted-foreground text-muted-foreground flex items-center justify-center relative">
