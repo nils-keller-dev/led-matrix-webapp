90dbf7c4b15552863629c8fc17569da178fa5792
tsomic <keller.nils99@gmail.com>
2024-04-07 20:27:33 +0200

    👷 add --no-delete flag to deploy scripts

--- deploy.bat
+++ deploy.bat
@@ -7,16 +7,29 @@ set PI_HOST=192.168.178.43
 set PI_TARGET_DIR=/home/admin/rpi-led-matrix-application/webapp
 set PSCP_PATH=pscp -i "C:\.ssh\LED.ppk"
 set PLINK_PATH=plink -i "C:\.ssh\LED.ppk"
+set DELETE_OLD_FILES=1
+
+:argloop
+if "%~1"=="" goto endargs
+if "%~1"=="--no-delete" set DELETE_OLD_FILES=0 & shift & goto argloop
+shift
+goto argloop
+:endargs
 
 echo Building the project...
 call npm run build
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 echo.
-echo Deleting old files on Raspberry Pi...
-echo y | %PLINK_PATH% -ssh %PI_USER%@%PI_HOST% "rm -rf %PI_TARGET_DIR%/*"
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-echo Done.
+
+if %DELETE_OLD_FILES%==1 (
+    echo Deleting old files on Raspberry Pi...
+    echo y | %PLINK_PATH% -ssh %PI_USER%@%PI_HOST% "rm -rf %PI_TARGET_DIR%/*"
+    if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+    echo Done.
+) else (
+    echo Skipping deletion of old files.
+)
 
 echo.
 echo Transferring files to Raspberry Pi...
--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,28 @@ BUILD_DIR="dist"
 PI_USER="admin"
 PI_HOST="192.168.178.43"
 PI_TARGET_DIR="/home/admin/rpi-led-matrix-application/webapp"
+DELETE_OLD_FILES=true
+
+for arg in "$@"
+do
+    if [[ $arg == "--no-delete" ]]; then
+        DELETE_OLD_FILES=false
+        break
+    fi
+done
 
 echo "Building the project..."
 npm run build || { echo "Build failed"; exit 1; }
 
 echo
 
-echo "Deleting old files on Raspberry Pi..."
-ssh $PI_USER@$PI_HOST "rm -rf $PI_TARGET_DIR/*" || { echo "Failed to delete old files"; exit 1; }
-
-echo "Done."
+if [ "$DELETE_OLD_FILES" = true ]; then
+    echo "Deleting old files on Raspberry Pi..."
+    ssh $PI_USER@$PI_HOST "rm -rf $PI_TARGET_DIR/*" || { echo "Failed to delete old files"; exit 1; }
+    echo "Done."
+else
+    echo "Skipping deletion of old files."
+fi
 
 echo
 
