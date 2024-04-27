619be382260a98c64ff746e81fd1ae4467e24e4c
tsomic <keller.nils99@gmail.com>
2024-04-27 09:24:35 +0200

    🚸 prevent closing Dialog on click outside

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -11,7 +11,10 @@ export function Dialog({ children, open, onOpenChange }: DialogProps) {
     <RadixDialog.Root open={open} onOpenChange={onOpenChange}>
       <RadixDialog.Portal>
         <RadixDialog.Overlay className="backdrop-brightness-50 backdrop-blur-sm size-screen fixed top-0 z-30" />
-        <RadixDialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12 bg-background p-4 rounded-xl outline-none z-40">
+        <RadixDialog.Content
+          onPointerDownOutside={(e) => e.preventDefault()}
+          className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-10/12 bg-background p-4 rounded-xl outline-none z-40"
+        >
           {children}
         </RadixDialog.Content>
       </RadixDialog.Portal>
