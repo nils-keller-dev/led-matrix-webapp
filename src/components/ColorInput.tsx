654d980bf94922eefb6d0dc81d1980fea2e6858d
tsomic <keller.nils99@gmail.com>
2024-05-17 12:03:35 +0200

    ✨ use Skeleton in COlorInput

--- src/components/ColorInput.tsx
+++ src/components/ColorInput.tsx
@@ -1,6 +1,7 @@
 import { useSignal } from '@preact/signals'
-import { useEffect, useRef, useCallback } from 'preact/hooks'
 import debounceFunction from 'debounce-fn'
+import { useCallback, useEffect, useRef } from 'preact/hooks'
+import { Skeleton } from './Skeleton'
 
 type ColorInputProps = {
   initialValue: string
@@ -10,20 +11,23 @@ type ColorInputProps = {
 export function ColorInput({ initialValue, onChange }: ColorInputProps) {
   const colorInput = useRef<HTMLInputElement>(null)
 
-  const currentColor = useSignal(initialValue)
+  const displayColor = useSignal(initialValue)
+  const colorValue = useSignal(initialValue)
   const colorName = useSignal('')
 
   const onColorChange = () => {
     const color = colorInput.current?.value ?? ''
-    if (!color || color === currentColor.value) return
+    if (!color || color === colorValue.value) return
 
-    currentColor.value = color
-    debouncedOnChange(currentColor.value)
+    displayColor.value = color
+    colorName.value = ''
+    debouncedOnChange(color)
   }
 
   const debouncedOnChange = useCallback(
     debounceFunction(
       (color: string) => {
+        colorValue.value = color
         onChange?.(color)
       },
       { wait: 500 }
@@ -37,27 +41,30 @@ export function ColorInput({ initialValue, onChange }: ColorInputProps) {
 
   useEffect(() => {
     fetch(
-      `https://www.thecolorapi.com/id?hex=${currentColor.value.replace('#', '')}`
+      `https://www.thecolorapi.com/id?hex=${colorValue.value.replace('#', '')}`
     )
       .then((response) => response.json())
       .then((data) => (colorName.value = data.name.value))
-  }, [currentColor.value])
+  }, [colorValue.value])
 
   return (
     <div className="w-full py-3 rounded-md border border-secondary bg-background flex items-center relative">
       <label htmlFor="picker" className="size-full absolute" />
       <div
         className="size-6 rounded-md border border-secondary ml-4"
-        style={{ backgroundColor: currentColor.value }}
+        style={{ backgroundColor: displayColor.value }}
       />
       <input
         id="picker"
         aria-label="color picker"
         ref={colorInput}
         type="color"
-        value={currentColor.value}
+        value={colorValue.value}
         className="opacity-0 size-0"
       />
+      {colorName.value ? null : (
+        <Skeleton className="w-[100px] h-[20px] ml-3" />
+      )}
       <span className="ml-3 text-primary">{colorName.value}</span>
     </div>
   )
