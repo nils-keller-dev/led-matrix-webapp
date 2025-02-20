743f1c47b4df2e421e60fbdb7419ba1c1d1740e5
tsomic <keller.nils99@gmail.com>
2025-02-20 21:38:13 +0100

    ✅ add tests for IconButton

---
+++ tests/components/IconButton.spec.tsx
@@ -0,0 +1,33 @@
+import { render } from '@testing-library/preact'
+import { describe, expect, test, vi } from 'vitest'
+import { IconButton } from '../../src/components/IconButton'
+
+describe('IconButton', () => {
+  test('renders with className props', () => {
+    const { container } = render(
+      <IconButton className="className1 className2" />
+    )
+    expect(container.firstElementChild?.classList).toContain('className1')
+    expect(container.firstElementChild?.classList).toContain('className2')
+  })
+
+  test('works with generic props', () => {
+    const onClick = vi.fn()
+
+    const { container } = render(
+      <IconButton aria-label="customAriaLabel" onClick={onClick} />
+    )
+    expect(container.firstElementChild?.getAttribute('aria-label')).toBe(
+      'customAriaLabel'
+    )
+  })
+
+  test('renders children', () => {
+    const { container } = render(
+      <IconButton>
+        <span>Icon</span>
+      </IconButton>
+    )
+    expect(container.textContent).toContain('Icon')
+  })
+})
