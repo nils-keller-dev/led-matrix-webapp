e4b356a056d8d52baa466d278a3cfad894595e07
tsomic <keller.nils99@gmail.com>
2025-02-20 23:35:26 +0100

    ✅ add tests for ColorConversion

---
+++ tests/utils/ColorConversion.spec.ts
@@ -0,0 +1,28 @@
+import { hexToRgb, rgbToHex } from '../../src/utils/ColorConversion'
+import { describe, it, expect, test } from 'vitest'
+
+const colors = [
+  { rgb: [255, 0, 0], hex: '#ff0000' },
+  { rgb: [0, 255, 0], hex: '#00ff00' },
+  { rgb: [0, 0, 255], hex: '#0000ff' },
+  { rgb: [255, 255, 255], hex: '#ffffff' },
+  { rgb: [0, 0, 0], hex: '#000000' }
+]
+
+describe('ColorConversion', () => {
+  describe('rgbToHex', () => {
+    test('converts correctly', () => {
+      colors.forEach((color) => {
+        expect(rgbToHex(color.rgb)).toBe(color.hex)
+      })
+    })
+  })
+
+  describe('hexToRgb', () => {
+    it('converts correctly', () => {
+      colors.forEach((color) => {
+        expect(hexToRgb(color.hex)).toEqual(color.rgb)
+      })
+    })
+  })
+})
