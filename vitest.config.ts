ded2da81bc8093430624804ff451bb6f44483212
tsomic <keller.nils99@gmail.com>
2025-02-22 10:14:11 +0100

    🔧 remove constants from test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vitest/config'
+import { coverageConfigDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     environment: 'happy-dom',
-    setupFiles: './vitest.setup.ts'
+    setupFiles: './vitest.setup.ts',
+    coverage: {
+      exclude: [
+        '**/enums/**',
+        '**/interfaces/**',
+        '**/constants/**',
+        ...coverageConfigDefaults.exclude
+      ]
+    }
   }
 })
