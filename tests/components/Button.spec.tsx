99a1e6096bbc2fdde8e22f2616e383563f3586e3
tsomic <keller.nils99@gmail.com>
2025-03-26 19:09:28 +0100

    ♻️ refactor Button tests using enzyme

--- tests/components/Button.spec.tsx
+++ tests/components/Button.spec.tsx
@@ -1,33 +1,33 @@
-import { fireEvent, render } from '@testing-library/preact'
+import { mount, shallow } from 'enzyme'
 import { describe, expect, test, vi } from 'vitest'
 import { Button } from '../../src/components/Button'
 
 describe('Button', () => {
+  test('matches snapshot', () => {
+    const wrapper = mount(<Button text="Button text" onClick={() => {}} />)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
   test('renders with text', () => {
-    const { container } = render(
-      <Button text="Button text" onClick={() => {}} />
-    )
-    expect(container.firstElementChild?.classList).toContain('bg-primary')
-    expect(container.textContent).toContain('Button text')
+    const wrapper = shallow(<Button text="Button text" onClick={() => {}} />)
+
+    expect(wrapper.hasClass('bg-primary')).toBe(true)
+    expect(wrapper.text()).toContain('Button text')
   })
 
   test('applies correct secondary classes', () => {
-    const { container } = render(
+    const wrapper = shallow(
       <Button text="Button text" isSecondary onClick={() => {}} />
     )
 
-    expect(container.firstElementChild?.classList).toContain(
-      'border-muted-foreground'
-    )
+    expect(wrapper.hasClass('border-muted-foreground')).toBe(true)
   })
 
   test('calls onClick when clicked', () => {
     const onClick = vi.fn()
-    const { container } = render(
-      <Button text="Button text" onClick={onClick} />
-    )
+    const wrapper = shallow(<Button text="Button text" onClick={onClick} />)
 
-    fireEvent.click(container.firstElementChild as Element)
+    wrapper.simulate('click')
     expect(onClick).toHaveBeenCalled()
   })
 })
