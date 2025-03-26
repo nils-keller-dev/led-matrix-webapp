48092224f2a7f039f481357943e13b31e29ed922
tsomic <keller.nils99@gmail.com>
2025-03-26 19:01:41 +0100

    ➕ add enzime for unit testing

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,5 +1,9 @@
 import { cleanup } from '@testing-library/preact'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-preact-pure'
 import { afterEach } from 'vitest'
+
+configure({ adapter: new Adapter() })
 
 afterEach(() => {
   cleanup()
